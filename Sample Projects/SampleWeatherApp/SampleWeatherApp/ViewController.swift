43ae6b0be75e82e83ca79ab6886cac1e1a644a40
LeeNamJun <dlskawns96@gmail.com>
2021-05-24 16:55:20 +0900

    new sample project

--- Sample Projects/SampleWeatherApp/SampleWeatherApp/ViewController.swift
+++ Sample Projects/SampleWeatherApp/SampleWeatherApp/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var iconImageView: UIImageView!
+    @IBOutlet var tempLabel: UILabel!
+    @IBOutlet var maxTempLabel: UILabel!
+    @IBOutlet var minTempLabel: UILabel!
+    
+    var weather: Weather?
+    var main: Main?
+    var name: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        WeatherService().getWeather { result in
+            switch result {
+            case .success(let weatherResponse):
+                DispatchQueue.main.async {
+                    self.weather = weatherResponse.weather.first
+                    self.main = weatherResponse.main
+                    self.name = weatherResponse.name
+                    self.setWeatherUI()
+                }
+            case .failure(_ ):
+                print("error")
+            }
+        }
     }
-
-
+    
+    private func setWeatherUI() {
+        let url = URL(string: "https://openweathermap.org/img/wn/\(self.weather?.icon ?? "00")@2x.png")
+        let data = try? Data(contentsOf: url!)
+        if let data = data {
+            iconImageView.image = UIImage(data: data)
+        }
+        tempLabel.text = "\(main!.temp)"
+        maxTempLabel.text = "\(main!.temp_max)"
+        minTempLabel.text = "\(main!.temp_min)"
+    }
 }
 
---
+++ Sample Projects/SampleWeatherApp/SampleWeatherApp/Weather.swift
@@ -0,0 +1,29 @@
+//
+//  Weather.swift
+//  SampleWeatherApp
+//
+//  Created by Lee Nam Jun on 2021/05/24.
+//
+
+import Foundation
+
+struct WeatherResponse: Decodable {
+    let weather: [Weather]
+    let main: Main
+    let name: String
+}
+
+struct Main: Decodable {
+    let temp: Double
+    let temp_min: Double
+    let temp_max: Double
+}
+
+struct Weather: Decodable {
+    let id: Int
+    let main: String
+    let description: String
+    let icon: String
+}
+
+
