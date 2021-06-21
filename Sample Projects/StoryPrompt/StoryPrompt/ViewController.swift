c6380f2fbee5025b7a258abbacea44465d04afd6
LeeNamJun <dlskawns96@gmail.com>
2021-06-21 18:53:19 +0900

    ~ Dismiss Keyboard

--- Sample Projects/StoryPrompt/StoryPrompt/ViewController.swift
+++ Sample Projects/StoryPrompt/StoryPrompt/ViewController.swift
@@ -8,17 +8,58 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var nounTextField: UITextField!
+    @IBOutlet var adjectiveTextField: UITextField!
+    @IBOutlet var verbTextField: UITextField!
+    @IBOutlet var numberSlider: UISlider!
+    @IBOutlet var numberLabel: UILabel!
+    
+    let storyPrompt = StoryPromptEntry()
+    
+    @IBAction func changeNumber(_ sender: UISlider) {
+        numberLabel.text = "Number: \(Int(sender.value))"
+        storyPrompt.number = Int(sender.value)
+    }
+    
+    @IBAction func changeStoryType(_ sender: UISegmentedControl) {
+        if let genre = StoryPrompts.Genre(rawValue: sender.selectedSegmentIndex) {
+            storyPrompt.genre = genre
+        } else {
+            storyPrompt.genre = .scifi
+        }
+    }
+    @IBAction func generateStoryPrompt(_ sender: Any) {
+        updateStoryPrompt()
+        print(storyPrompt)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let storyPrompt = StoryPromptEntry()
+        
+        numberSlider.value = 7.5
         storyPrompt.noun = "toaster"
         storyPrompt.adjective = "smelly"
         storyPrompt.verb = "burps"
-        storyPrompt.number = 10
+        storyPrompt.number = Int(numberSlider.value)
         print(storyPrompt)
+        
+        nounTextField.delegate = self
+        verbTextField.delegate = self
+        adjectiveTextField.delegate = self
     }
-
-
+    
+    func updateStoryPrompt() {
+        storyPrompt.noun = nounTextField.text ?? ""
+        storyPrompt.verb = verbTextField.text ?? ""
+        storyPrompt.adjective = adjectiveTextField.text ?? ""
+    }
 }
 
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        updateStoryPrompt()
+        return true
+    }
+}
